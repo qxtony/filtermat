22a8ebcf506d22ca283a5374bd201ae9001449a8
qxtony <71513188+qxtony@users.noreply.github.com>
2022-06-11 22:55:41 +0700

    Update filter.py
    Fixed type annotations.

--- roughfilter/functions/filter.py
+++ roughfilter/functions/filter.py
@@ -1,10 +1,11 @@
 from typing import List, Optional
 
 from roughfilter.bases import kit_rough_words
+from roughfilter.functions.word import Word
 
 
 class RoughFilter:
-    def __init__(self, word: str) -> None:
+    def __init__(self, word: Word) -> None:
         self.word = word
 
     def get_rough_words(self) -> List[str]:
@@ -13,21 +14,14 @@ class RoughFilter:
     def find_vulgar_words(self) -> Optional[str]:
 
         word = (
-            self.word
-            .replace_identically_looking_letters()
-            .revert_transcription()
+            self.word.replace_identically_looking_letters().revert_transcription()
         )
 
         if response := word.search_in_kit_words(self.get_rough_words()):
             return response
 
-        elif response := (
-            word
-            .to_base_form()
-            .find_vulgar_words()
-        ):
+        elif response := (word.to_base_form().find_vulgar_words()):
             return response
- 
 
-    def overall_performance(self) -> str:
+    def overall_performance(self) -> Optional[str]:
         return self.find_vulgar_words()
