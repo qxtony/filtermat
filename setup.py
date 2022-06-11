9717b1ce67434a474ef8c4de289dfaa74617a670
qxtony <71513188+qxtony@users.noreply.github.com>
2022-06-11 23:01:37 +0700

    Update setup.py
    Updated the old one setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 
 
-with open("README.md", "r") as file:
-	long_description = file.read()
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
 
 requirements = ["pymorphy2", "pymorphy2-dicts-ru", "regex"]
 
 setup(
-	name="filtermat",
-	version="1.5.3",
-	author="qxtony",
-	author_email="AntonSharafiev3866@gmail.com",
-	description="This Python library is needed in order to search for Russian mat in text.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/qxtony/filtermat",
-	packages=["filtermat", "filtermat/files", "filtermat/functions"],
-	install_requires=requirements,
-	classifiers=[
-		"Programming Language :: Python :: 3.8",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-	python_requires='>=3.6',
+    name="roughfilter",
+    version="2.0.2",
+    author="qxtony",
+    author_email="qxtonydev@gmail.com",
+    description="With this library, you can easily catch rude expressions in text.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qxtony/roughfilter",
+    packages=["roughfilter", "roughfilter/bases", "roughfilter/functions"],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
-
