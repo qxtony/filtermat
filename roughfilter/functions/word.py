b2850f5d9e25a6bedeb2f7cdfd3a1555208f4390
qxtony <71513188+qxtony@users.noreply.github.com>
2022-06-11 22:57:32 +0700

    Update word.py
    Type annotations have been fixed, and unnecessary conditions have been removed.

--- roughfilter/functions/word.py
+++ roughfilter/functions/word.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, TypeVar
 
 from regex import findall
 
@@ -10,36 +10,41 @@ from roughfilter.configurations import (
 )
 
 
+T_Word = TypeVar("T_Word", bound="Word")
+
+
 class Word:
     def __init__(self, word: str) -> None:
         self.word_tested = word
 
-    def to_base_form(self) -> str:
+    def to_base_form(self) -> T_Word:
         parsed_word = morph.parse(self.word_tested)[0]
         self.word_tested = parsed_word.normal_form
 
         return self
 
-    def replace_identically_looking_letters(self) -> str:
+    def replace_identically_looking_letters(self) -> T_Word:
         def get_symbol(symbol: str) -> str:
             return transcriptions.get(symbol) or symbol
 
         self.word_tested = "".join(map(get_symbol, self.word_tested))
         return self
 
-    def revert_transcription(self) -> str:
+    def revert_transcription(self) -> T_Word:
         def get_symbol(symbol: str) -> str:
             return translations.get(symbol) or symbol
 
         self.word_tested = "".join(map(get_symbol, self.word_tested))
         return self
 
-    def find_vulgar_words(self) -> List[str]:
+    def find_vulgar_words(self) -> Optional[T_Word]:
         if result := findall(re_filter, self.word_tested):
             self.word_tested = result
             return self
 
-    def is_startswith_or_endswith(self, rough_words: List[str]) -> bool:
+    def is_startswith_or_endswith(
+        self, rough_words: List[str]
+    ) -> Optional[bool]:
         for word in rough_words:
             if (
                 self.word_tested.startswith(word) or
@@ -47,10 +52,9 @@ class Word:
             ):
                 return True
 
-    def search_in_kit_words(self, rough_words: List[str]):
-        if self.word_tested in rough_words:
+    def search_in_kit_words(self, rough_words: List[str]) -> Optional[T_Word]:
+        if (
+            self.is_startswith_or_endswith(rough_words)
+            or self.word_tested in rough_words
+        ):
             return self
-
-        elif self.is_startswith_or_endswith(rough_words):
-            return self
- 
